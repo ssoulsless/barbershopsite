9ba14e6f4a35ba00f766bf289e380d52ef6b0aac
silversoulsless <doodlezzzfobozzz@gmail.com>
2021-02-14 23:04:46 +0300

    slides work correctly

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,43 @@
-const toggle = $(".slider__toggle");
-const slider__item = $(".slider__item");
+let toggle;
+let slider__item;
+let isSlicked = false;
+
 $(document).ready(function () {
-    $('.slider__list').slick({
-        slider__item: 3
-    });
+    if ($(document).width() < 768) {
+        slickify();
+    };
 });
 
-$(".slider__list").on('slideChange', function (event, slick, currentSlide){
-    $(".slider__toggle--current").removeClass(".slider__toggle--current");
-    toggle[0].addClass(".slider__toggle--current");
-    console.log("fire");
+$(window).resize(function () {
+    let $windowWidth = $(document).width();
+    if (($windowWidth >= 768) & (isSlicked)) {
+        $('.slider__list').slick("unslick");
+        isSlicked = false;
+    }
+    if (($windowWidth < 767) & (!isSlicked)) {
+        slickify();
+    };
 });
+
+$('.slider__list').on('swipe', function () {
+    for (let n = 0; n < toggle.length; n++) {
+        slider__item[n].classList.remove("slider__item--current");
+        toggle[n].src = "./img/slider indicator.svg";
+        toggle[n].classList.remove("slider__toggle--current");
+    };
+    let i = $('.slider__list').slick('slickCurrentSlide');
+    toggle[i].classList.add("slider__toggle--current");
+    toggle[i].src = "./img/current slider.svg";
+    slider__item[i].classList.add("slider__item--current");
+});
+
+function slickify() {
+    isSlicked = true;
+    toggle = $(".slider__toggle");
+    slider__item = $(".slider__item");
+    $('.slider__list').slick({
+        slider__item: 3,
+        infinite: false,
+        mobileFirst: true
+    });
+}
